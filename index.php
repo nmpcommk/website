5ff3c781a6f336d5be79c66ea7b7731b0e2eb815
User <ivanovski@gmail.com>
2023-11-20 00:50:13 +0100

    All except Galerii

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+
+<html>
+
+<head>
+
+<title>:: Summer 2008 :: Bureau for Tourism of Republic of Melancholia</title>
+
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
+
+<meta name="description" content="НМП е акција сама по себе, движење кон ѕид без ниедна кочница на тркалата">
+
+    <meta name="KEYWORDS" content="nmp, digital, art, digitalna, umetnost, skopje, makedonija, macedonia, galerii, galleries, mudri jarci, deca od guma, doppelganger, dopelganger, flavrsavr, serdarot, online art">
+
+<link rel="stylesheet" type="text/css" href="nmp.css">
+
+<link rel="SHORTCUT ICON" href="sliki/zanaweb.ico">
+
+<style type="text/css">
+
+<!--
+
+a:link {
+
+	text-decoration: none;
+
+}
+
+a:visited {
+
+	text-decoration: none;
+
+}
+
+a:hover {
+
+	text-decoration: none;
+
+}
+
+a:active {
+
+	text-decoration: none;
+
+}
+body {
+	background-color: #FFFFFF;
+	background-image: url(../../Sandbox/Drafts/patagonia.jpg);
+}
+
+-->
+
+</style>
+
+</head>
+
+
+
+<body>
+<div align="center">
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+</div>
+</body>
+
+</html>
+
+<!-- posledna izmena - 26 fevruari 2006 - Ivica -->
